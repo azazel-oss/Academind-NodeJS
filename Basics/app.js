a9d64e7c39097d070c6ef5120a2b1d61116d0472
Asad <asadmhmd1234@gmail.com>
2022-01-08 18:25:32 +0530

    Add templates

--- Basics/app.js
+++ Basics/app.js
@@ -1,26 +1,24 @@
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-const rootDir = require("./util/path");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const adminRouter = require("./routes/admin");
-const shopRouter = require("./routes/shop");
 
-// app.use("/", (req, res, next) => {
-//   console.log("This always runs");
-//   next();
-// });
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", adminRouter);
-app.use(shopRouter);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  console.log(rootDir);
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
